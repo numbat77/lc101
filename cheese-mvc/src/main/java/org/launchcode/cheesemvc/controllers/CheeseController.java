883432336157ae0641d21d679fe90c7a7af1e8ae
sarah <sarah.hanley@outlook.com>
2017-11-16 19:39:32 -0600

    initial commit

--- cheese-mvc/src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
+++ cheese-mvc/src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -7,12 +7,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 @Controller
 @RequestMapping("cheese")
 public class CheeseController {
 
-    static ArrayList<String> cheeses = new ArrayList<>();
+    static HashMap<String, String> cheeses = new HashMap<>();
 
     // Request path: /cheese
     @RequestMapping(value = "")
@@ -26,11 +27,26 @@ public class CheeseController {
     @RequestMapping(value = "add", method = RequestMethod.GET)
     public String displayAddCheeseForm(Model model) {
         model.addAttribute("title", "Add Cheese");
-        return "cheese/add";
+        return "cheese/addCheese";
     }
     @RequestMapping(value="add", method = RequestMethod.POST)
-    public String processAddCheeseForm(@RequestParam String cheeseName) {
-        cheeses.add(cheeseName);
+    public String processAddCheeseForm(@RequestParam String cheeseName, @RequestParam String cheeseDescription) {
+        cheeses.put(cheeseName, cheeseDescription);
+
+        // Redirect to /cheese
+        return "redirect:";
+    }
+    @RequestMapping(value = "delete", method = RequestMethod.GET)
+    public String displayDeleteCheeseForm(Model model) {
+        model.addAttribute("cheeses", cheeses);
+        model.addAttribute("title", "Delete Cheese");
+
+        return "cheese/deleteCheese";
+    }
+    @RequestMapping(value="delete", method = RequestMethod.POST)
+    public String processDeleteCheeseForm(@RequestParam String deleteCheese) {
+
+        cheeses.remove(deleteCheese);
 
         // Redirect to /cheese
         return "redirect:";
