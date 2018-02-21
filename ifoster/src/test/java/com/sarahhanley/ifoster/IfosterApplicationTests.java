c4429bf830f19fd2aa86f9ea61f3dc690568171e
sarah <sarah.hanley@outlook.com>
2018-02-21 17:34:58 -0600

    initial commit

---
+++ ifoster/src/test/java/com/sarahhanley/ifoster/IfosterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sarahhanley.ifoster;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IfosterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
