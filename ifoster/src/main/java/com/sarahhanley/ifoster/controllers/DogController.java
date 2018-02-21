55e7bc3815434fe4e0e9dcff95bf8c3b90b1b32a
sarah <sarah.hanley@outlook.com>
2018-02-21 17:38:24 -0600

    project setup

---
+++ ifoster/src/main/java/com/sarahhanley/ifoster/controllers/DogController.java
@@ -0,0 +1,14 @@
+package com.sarahhanley.ifoster.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.xml.ws.RequestWrapper;
+
+@Controller
+public class DogController {
+    @RequestMapping("")
+//TODO
+    return index;
+
+}
